bd726c1c301c29c664fd737b9947bff6e193d8af
이유빈 <lyb991113@gmail.com>
2024-05-13 22:25:30 +0900

    [feat] Member Entity 클래스를 생성한다. - AgeGroup, Gender는 Enum클래스로 생성한다. - aplication.yml에 h2데이터베이스 설정 추가

---
+++ src/main/java/com/example/hufs/user/domain/entity/enumtype/AgeGroup.java
@@ -0,0 +1,22 @@
+package com.example.hufs.user.domain.entity.enumtype;
+
+import lombok.Getter;
+
+@Getter
+public enum AgeGroup {
+    TEEN("10대"),
+    TWENTY("20대"),
+    THIRTY("30대"),
+    FORTY("40대"),
+    FIFTY("50대"),
+    SIXTY("60대"),
+    SEVENTY("70대"),
+    EIGHTY("80대"),
+    NINETY("90대"),
+    HUNDRED("100대");
+
+    private final String type;
+
+    AgeGroup(String type) { this.type = type; }
+
+}
---
+++ src/main/java/com/example/hufs/user/domain/entity/enumtype/Gender.java
@@ -0,0 +1,7 @@
+package com.example.hufs.user.domain.entity.enumtype;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    SECRET //성별을 알리기 싫은 경우
+}

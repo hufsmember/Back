2468594884173b1911c318d53ff5ec897bb90b47
이유빈 <lyb991113@gmail.com>
2024-05-14 16:08:24 +0900

    [fix] 공통 클래스를 생성한다. - AppConfig: 기본 설정 클래스(빈 등록 등등 - BaseResponseDTO: AppConfig 클래스에서 ObjectMapper를 빈으로 등록하여 전역적으로 JsonInclude.Include.NON_NULL 옵션을 설정해 주었기 때문에 data에 붙어있던 어노테이션을 삭제해 주었음

---
+++ src/main/java/com/example/hufs/common/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.example.hufs.common.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+빈 등록 등 단순 설정을 모아두는 클래스
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); //null값이 포함된 속성은 json으로 바꾸지 않도록
+        return objectMapper;
+    }
+}

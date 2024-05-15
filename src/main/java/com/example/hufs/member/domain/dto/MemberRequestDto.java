67902a29cf7b23d6fe85f190cb41f8459a6991b8
이유빈 <lyb991113@gmail.com>
2024-05-15 22:02:35 +0900

    [feat] Security 기본 설정을 한다. - MemberRepository: 로그인과 회원 가입을 위해 이메일이 DB에 있는지와 findByEmail을 한다. - MemberService: Member의 비즈니스 로직을 작성한다. 회원가입 시 유효성 검사를 통해 DB에 저장한다. 로그인 시 토큰을 응답값으로 보내준다. - MemberRequestDto: 회원가입 시 필요한 요청 DTO를 생성한다. - MemberLoginRequestDto: 로그인 시 필요한 요청 DTO를 생성한다.

---
+++ src/main/java/com/example/hufs/member/domain/dto/MemberRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.hufs.member.domain.dto;
+
+public record MemberRequestDto(
+        String name,
+        String email,
+        String password,
+        Boolean isFamilyHave
+) {
+}

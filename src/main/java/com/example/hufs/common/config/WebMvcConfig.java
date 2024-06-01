027d215c4a973e5e7649e95e9db0e127df938bf4
이유빈 <lyb991113@gmail.com>
2024-06-01 22:35:50 +0900

    [Refactor] 모든 url에서 접속 허용되도록 변경

--- src/main/java/com/example/hufs/common/config/WebMvcConfig.java
+++ src/main/java/com/example/hufs/common/config/WebMvcConfig.java
@@ -6,17 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
-                .maxAge(3600);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
+                .allowedOriginPatterns("*")  // 모든 오리진 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")  // 허용할 HTTP 메서드 지정
+                .allowedHeaders("*")      // 모든 헤더 허용
+                .allowCredentials(true);    // JWT 사용을 위한 자격 증명 허용
     }
 }

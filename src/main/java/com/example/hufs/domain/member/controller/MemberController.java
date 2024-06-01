b172b53d9cfe00ddc988706ea2aaf30945c1d4d5
이유빈 <lyb991113@gmail.com>
2024-06-01 23:18:26 +0900

    [Refactor] 사용하지 않는 import문 제거

--- src/main/java/com/example/hufs/domain/member/controller/MemberController.java
+++ src/main/java/com/example/hufs/domain/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.example.hufs.common.exception.ErrorCode;
 import com.example.hufs.common.response.BaseResponseDTO;
 import com.example.hufs.common.security.principal.MemberDetail;
 import com.example.hufs.domain.member.dto.*;
+import com.example.hufs.domain.member.dto.response.MemberResponseDto;
 import com.example.hufs.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -81,5 +82,12 @@ public class MemberController {
 
         memberService.veganAndAllergy(requestDto, memberDetail.getUsername());
         return BaseResponseDTO.ok();
+    }
+
+    @GetMapping("/get/{member_id}")
+    public BaseResponseDTO<MemberResponseDto> getMember(
+            @PathVariable("member_id") Long id
+    ) {
+        return BaseResponseDTO.okWithData(memberService.getMember(id));
     }
 }

5e4fe505c79747cc79f247bc1bb41977fcd6dc32
이유빈 <lyb991113@gmail.com>
2024-06-03 19:34:30 +0900

    로그아웃 임시 명시로 변경

--- src/main/java/com/example/hufs/common/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/hufs/common/security/jwt/JwtAuthenticationFilter.java
@@ -35,9 +35,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     FilterChain filterChain
     ) throws ServletException, IOException {
         //header에서 토큰 값을 읽어와야 함.
-        String accessToken = request.getHeader("accessToken"); //header의 AUTHORIZATION 필드를 읽어옴.(해당 필드에는 토큰 값이 있음)
-        if (accessToken!=null&&tokenManager.isTokenBlacklisted(accessToken)){
-            Authentication authentication = getEmailPassword(accessToken); //받은 엑세스 토큰으로 사용자 인증 정보를 불러옴
+        String accessToken = request.getHeader("accessToken");
+        System.out.println("AccessToken: " + accessToken); // 토큰 로그 추가//header의 AUTHORIZATION 필드를 읽어옴.(해당 필드에는 토큰 값이 있음)
+        if (accessToken!=null){
+            Authentication authentication = getEmailPassword(accessToken);
+            System.out.println("Authentication: " + authentication);//받은 엑세스 토큰으로 사용자 인증 정보를 불러옴
 
             SecurityContextHolder.getContext()
                     .setAuthentication(authentication); //이 인증 정보를 security 필터에 전달.

d25b1abf1389fed37b327f6b46f697a205ae7db7
이유빈 <lyb991113@gmail.com>
2024-06-04 23:00:24 +0900

    api 연동을 위해 코드를 생성

--- src/main/java/com/example/hufs/backtoai/service/RecommendService.java
+++ src/main/java/com/example/hufs/backtoai/service/RecommendService.java
@@ -13,10 +13,10 @@ import com.example.hufs.domain.recipeIngrendient.entity.RecipeIngredient;
 import com.example.hufs.domain.recipeIngrendient.repository.RecipeIngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +25,7 @@ public class RecommendService {
     private final RecipeRepository recipeRepository;
     private final MemberRepository memberRepository;
     private final RecipeIngredientRepository recipeIngredientRepository;
+    private final WebClient webClient;
 
     public RecommendRecipeResponseDto getRecommendRecipe(String email) {
 
@@ -32,8 +33,37 @@ public class RecommendService {
                 .orElseThrow(()-> new BaseException(ErrorCode.MEMBER_NOT_EXIST));
 
         //AI로 api 요청하기
+        String url = "배포 url/api/recommend/recipe";
+
+        //요청받는 데이터 클래스를 따로 만들어줘야함
+        Mono<RecommendRecipeResponseDto> responseMono = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(url)
+                        .queryParam("member_id",member.getId())
+                        .build())
+                .retrieve()
+                .bodyToMono(RecommendRecipeResponseDto.class);
         //요청으로 받은 데이터(Json)를 내가 보기 좋게 변환하여 담는다.
-        //리스트dto로 넣을 거 넣고, 전체 결과는 Recipe테이블에 save
+        RecommendRecipeResponseDto recommendRecipeResponseDto = responseMono.block();
+
+//        if (recommendRecipeResponseDto != null) {
+//            List<Recipe> recipes = recommendRecipeResponseDto.getRecipes().stream().map(dto -> {
+//                Recipe recipe = new Recipe();
+//                recipe.setTitle(dto.getTitle());
+//                recipe.setDescription(dto.getDescription());
+//                recipe.setIngredients(dto.getIngredients().stream().map(ingredientDto -> {
+//                    RecipeIngredient ingredient = new RecipeIngredient();
+//                    ingredient.setName(ingredientDto.getName());
+//                    ingredient.setQuantity(ingredientDto.getQuantity());
+//                    return ingredient;
+//                }).collect(Collectors.toList()));
+//                // 필요한 다른 필드들 설정
+//                return recipe;
+//            }).collect(Collectors.toList());
+
+//        리스트 dto로 넣을 거 넣고, 전체 결과는 Recipe 테이블에 save
+//            recipeRepository.saveAll(recipes);
+
 
         return null;
     }
--- src/main/java/com/example/hufs/domain/product/service/ProductService.java
+++ src/main/java/com/example/hufs/domain/product/service/ProductService.java
@@ -2,12 +2,16 @@ package com.example.hufs.domain.product.service;
 
 import com.example.hufs.common.exception.BaseException;
 import com.example.hufs.common.exception.ErrorCode;
+import com.example.hufs.domain.member.entity.Member;
+import com.example.hufs.domain.member.repository.MemberRepository;
 import com.example.hufs.domain.product.dto.ProductListResponseDto;
 import com.example.hufs.domain.product.entity.Product;
 import com.example.hufs.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -17,6 +21,8 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
+    private final WebClient webClient;
 
     @Transactional(readOnly = true)
     public List<ProductListResponseDto> getAllProduct() {
@@ -34,5 +40,38 @@ public class ProductService {
                         .quantity(product.getPrice())
                         .build())
                 .toList();
+    }
+
+    public ProductListResponseDto getMyProduct(String email) {
+
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(()-> new BaseException(ErrorCode.MEMBER_NOT_EXIST));
+
+        //AI로 api 요청하기
+        String url = "배포 url/api/shinsegae/products";
+
+        //요청받는 데이터 클래스를 따로 만들어줘야함
+        Mono<ProductListResponseDto> responseMono = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(url)
+                        .queryParam("category","food")
+                        .build())
+                .retrieve()
+                .bodyToMono(ProductListResponseDto.class);
+        //요청으로 받은 데이터(Json)를 내가 보기 좋게 변환하여 담는다.
+        ProductListResponseDto productListResponseDto = responseMono.block();
+
+//        if (productListResponseDto != null) {
+//            List<Product> product = productListResponseDto.어쩌고.stream().map(dto -> {
+//
+//                }).collect(Collectors.toList()));
+//                // 필요한 다른 필드들 설정
+//
+//            }).collect(Collectors.toList());
+
+//        리스트 dto로 넣을 거 넣고, 전체 결과는 Recipe 테이블에 save
+//            productRepository.saveAll(recipes);
+
+        return null;
     }
 }
